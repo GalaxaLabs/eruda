d4ea2715999c7820939b5f042b7f3b3305266f86
GalaxaLabs <galaxa@zohomail.in>
2026-06-06 22:21:42 +0530

    fix: match export icon padding and box-sizing to native icons

--- src/Network/Network.js
+++ src/Network/Network.js
@@ -445,13 +445,15 @@ export default class Network extends Tool {
     this._$filterText = $el.find(c('.filter-text'))
     this._$control.find(c('.export-md')).css({
       position: 'absolute',
-      display: 'flex',
+      display: 'inline-flex',
       alignItems: 'center',
       justifyContent: 'center',
       cursor: 'pointer',
       top: '0',
-      bottom: '0',
-      width: '23px',
+      padding: '10px',
+      width: '36px',
+      height: '36px',
+      boxSizing: 'border-box',
       color: 'inherit'
     }).html('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor" width="16px" height="16px"><path d="M216,112v96a16,16,0,0,1-16,16H56a16,16,0,0,1-16-16V112A16,16,0,0,1,56,96h64v48a8,8,0,0,0,16,0V96h64A16,16,0,0,1,216,112ZM136,43.31l26.34,26.35a8,8,0,0,0,11.32-11.32l-40-40a8,8,0,0,0-11.32,0l-40,40a8,8,0,0,0,11.32,11.32L120,43.31V96a8,8,0,0,0,16,0Z"/></svg>')
   }
